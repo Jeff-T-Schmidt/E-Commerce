7d3a0611dc7423c39b673b1205d3655d9bb3e701
Jeff Schmidt <jeff.t.schmidt@gmail.com>
2022-05-04 21:48:29 -0700

    Wednesday PM

--- models/Product.js
+++ models/Product.js
@@ -23,7 +23,7 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
-      DECIMAL: (10,2),
+      // DECIMAL: (10,2),
       validate: {
         isDecimal: true
       }
